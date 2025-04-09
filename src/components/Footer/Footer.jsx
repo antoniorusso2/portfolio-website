287e70f50ddcb112ec122e679f2eb3efb7ac6ed6
antoniorusso2 <russoantonio303@outlook.it>
2025-04-09 20:18:19 +0200

    install axios and added project casrd component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,11 +4,39 @@ export default function Footer() {
       <footer>
         <div className="container">
           <div className="row">
-            <div className="contacts">
-              <h3>Contacts</h3>
-              <p>+7 999 999 99 99</p>
-              <p>9GvqO@example.com</p>
-            </div>
+            <ul className="contacts-list">
+              {/* phone */}
+              <li className="contacts-item">
+                <p className="contacts-text">
+                  Telefono: <span className="phone">+39 328-4879342</span>
+                </p>
+              </li>
+              {/* email */}
+              <li className="contacts-item">
+                <p className="contacts-text">
+                  Email: <span className="email">russoantonio303@outlook.it</span>
+                </p>
+              </li>
+            </ul>
+
+            {/* socials */}
+            <ul className="social-list">
+              <li className="social-item">
+                <a className="social-link" href="#" target="_blank" rel="noreferrer">
+                  <img src="https://img.icons8.com/ios-glyphs/30/ffffff/linkedin.png" alt="linkedin" />
+                </a>
+              </li>
+              <li className="social-item">
+                <a className="social-link" href="https://github.com/antoniorusso2" target="_blank" rel="noreferrer">
+                  <img src="https://img.icons8.com/ios-glyphs/30/ffffff/github.png" alt="github" />
+                </a>
+              </li>
+              <li className="social-item">
+                <a className="social-link" href="#" target="_blank" rel="noreferrer">
+                  <img src="https://img.icons8.com/ios-glyphs/30/ffffff/instagram-new.png" alt="instagram" />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </footer>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,21 @@
-import Carousel from '../components/Carousel/Carousel';
-
 export default function MainPAge() {
-  return <Carousel />;
+  return (
+    <div className="container">
+      <div className="row">
+        {/* hero section */}
+        <section className="hero-section">
+          <div className="hero">
+            <h1 className="hero-title">Antonio Russo</h1>
+            <p className="hero-subtitle">Junior Web Developer</p>
+          </div>
+        </section>
+        <section className="description-section">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, natus? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Quos, natus?
+          </p>
+        </section>
+      </div>
+    </div>
+  );
 }
